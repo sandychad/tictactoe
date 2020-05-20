500f5ef13ccf6645d8cd722d26ed13c19587e531
sandychad <sandychad@outlook.com>
2020-05-20 12:53:07 -0500

    First commit
    Basic tictactoe React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Game from "./components/Game";
+import Header from "./components/Header";
+
+const App = () => (
+  <div>
+    <Header />
+    <Game />
+  </div>
+);
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const style = {
+  textAlign: "center",
+};
+
+const Header = () => {
+  return (
+    <header style={style}>
+      <h1>Tic-Tac-Toe</h1>
+    </header>
+  );
+};
+
+export default Header;
